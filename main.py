5e80ac313fa94c3ab1be415e48f4d9401a053adb
SushiSquiid <charlie.a.griffiths@gmail.com>
2024-12-23 17:42:05 +0000

    added new function to find most used word and times it was used

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-def get_book_contents(boot_path):
+def get_book_contents():
     with open(book_path) as f:
         return f.read()
 
 def word_counter(book_contents):
     words = book_contents.split()
     return len(words)
+
+def common_words(book_contents):
+    words = book_contents.lower().split()
+    most_used_word = ""
+    times_word_used = float("-inf")
+    count = {}
+
+    for word in words:
+        if word not in count:
+            count[word] = 0
+        count[word] += 1
+
+        if count[word] > times_word_used:
+            times_word_used = count[word]
+            most_used_word = word
+    return most_used_word, times_word_used
 
 def character_counter(book_contents):
     book_contents = book_contents.lower()
@@ -21,14 +37,16 @@ def character_counter(book_contents):
 
 def book_report(book_path):
 
-    book_content = get_book_contents(book_path)
+    book_content = get_book_contents()
     word_count = word_counter(book_content)
     num_of_character = character_counter(book_content)
+    most_common_word, times_word_used = common_words(book_content)
 
     print(f"--- Begin report of {book_path} ---")
     print(f"   There are a total of {word_count} words in this book")
+    print(f"The most used word is '{most_common_word}', it was used {times_word_used} times")
     for letter, count in num_of_character.items():
-        print(f"    The '{letter}' character was found {count} times")
+        print(f"    The '{letter}' character was used {count} times")
     print(f"--- End of report ---")
     return book_report
 
